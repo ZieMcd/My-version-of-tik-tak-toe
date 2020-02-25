5e1070951fb1ac488fb320c218c8e17e2377aee5
Zie Mcdowell <59741700+ZieMcd@users.noreply.github.com>
2020-02-25 14:28:07 +0200

    Add files via upload

---
+++ tiktak/tiktak/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace tiktak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(12, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(152, 146);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(12, 164);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(152, 146);
+            this.b4.TabIndex = 1;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(170, 12);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(152, 146);
+            this.b2.TabIndex = 2;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(170, 164);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(152, 146);
+            this.b5.TabIndex = 3;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(328, 12);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(152, 146);
+            this.b3.TabIndex = 4;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(328, 164);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(152, 146);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(12, 316);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(152, 146);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(170, 316);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(152, 146);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(328, 316);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(152, 146);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(213, 468);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 492);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Tik Tak Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ tiktak/tiktak/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tiktak
+{
+    public partial class Form1 : Form
+    {
+
+        int win = 0;
+        public int i = 0;
+        public string[] player = new string[10] { "X", "O", "X", "O", "X", "O", "X", "O","X","O" };
+        public string[,] board = new string[3,3];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Press(b1,0,0);
+        }
+
+
+        public void Press(Button btnName, int y, int x)
+        {
+
+            
+
+            btnName.Text = player[i];
+            
+            btnName.Enabled = false;
+
+
+            board[y, x] = player[i];
+
+
+            //-------------------Cheking Row--------------------//
+            for (int j = 0; j<3; j++)
+            {
+                if (board[y,j ]== player[i])
+                {
+                    win++;
+                }
+            }
+
+
+
+            if (win == 3)
+            {
+                MessageBox.Show(player[i] + " has won");
+                Application.Exit();
+            } else { win = 0; }
+            //-------------------------------------------------//
+
+
+
+
+            //-------------------Cheking Columb--------------------//
+            for (int k=0; k<3; k++)
+            {
+                if (board[k, x] == player[i])
+                {
+                    win++;
+                }
+            }
+
+
+
+            if (win == 3)
+            {
+                MessageBox.Show(player[i] + " has won");
+                Application.Exit();
+            }
+            else { win = 0; }
+            //-----------------------------------------------------//
+
+
+
+
+
+            //--------------------Checking Diag--------------------//
+
+            if (board[0,0]==player[i] && board[1, 1] == player[i] && board[2, 2] == player[i])
+            {
+                MessageBox.Show(player[i] + " has won");
+                Application.Exit();
+            }
+
+            if (board[0, 2] == player[i] && board[1, 1] == player[i] && board[2, 0] == player[i])
+            {
+                MessageBox.Show(player[i] + " has won");
+                Application.Exit();
+            }
+
+
+            //-----------------------------------------------------//
+
+            i++;
+        }
+
+      
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Press(b2,0,1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Press(b3, 0, 2) ;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Press(b4,1,0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Press(b5,1,1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Press(b6,1,2);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Press(b7,2,0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Press(b8,2,1);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Press(b9,2,2);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                item.Enabled = true;
+
+                if (item.Name == "btnReset")
+                {
+                    MessageBox.Show("done");
+                    i = 0;
+                    win = 0;
+                    Array.Clear(board, 0, 3 * 3);
+
+                } else { item.Text = ""; }
+
+                
+            }
+        }
+    }
+}
